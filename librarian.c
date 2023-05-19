fa7a410cb3bfdf5857c3cde3006ede2d72d79013
blackcowman <soh990301@gmail.com>
2023-05-19 10:52:24 +0900

    modify usermenu, librarianmenu

--- librarian.c
+++ librarian.c
@@ -1,2 +1,24 @@
 #include "library.h"
 
+int addBook(Book *b);
+void readBook(Book b);
+void listBook(Book *b[], int count);
+int deleteBook(Book *b);
+
+
+
+int selectDataNo(Book *b[], int count);
+int selectBook();
+void saveBook(Book *b[], int count);
+int loadBook(Book *b[]);
+void searchBook(Book *s[], int count);
+void librarianmenu(){
+    printf("\n***도서관 이용 시스템(libraian)***\n");
+    printf("1. 신규 도서 등록\n");
+    printf("2. 도서 삭제\n");
+    printf("3. 도서 업데이트\n");
+    printf("4. 도서 목록\n");
+    printf("5. 도서 검색\n");
+    printf("0. 로그아웃\n");
+    printf(">> ");
+};
--- user.c
+++ user.c
@@ -2,7 +2,15 @@
 
 int borrowBook(Book *b);
 int reservationBook(Book *b);
-void usermenu(){
 
-    
+void usermenu(){
+    printf("\n***도서관 이용 시스템(user)***\n");
+    printf("1. 도서 목록\n");
+    printf("2. 도서 대출\n");
+    printf("3. 도서 반납\n");
+    printf("4. 도서 검색\n");
+    printf("5. 도서 예약\n");
+    printf("6. 이주의 책 보기\n");
+    printf("0. 로그아웃\n");
+    printf(">> ");
 }
